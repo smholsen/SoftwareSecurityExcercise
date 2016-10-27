e25b39d6af7e24a5e3abc19f72453d64293e4f28
Vrudin <vegarru@gmail.com>
2016-10-27 11:19:32 +0200

    Added requirement to be logged in to search

--- src/webapp/controllers/SearchController.php
+++ src/webapp/controllers/SearchController.php
@@ -11,29 +11,35 @@ class SearchController extends Controller
 	public function search()
 
     {
-
-    	$result = [];
-    	$needle  = $this->app->request->post('searchquery');
-        $patent = $this->patentRepository->all();
-
-        $validation = new SearchFormValidation($needle);
-        if ($validation->isGoodToGo()) 
+        if ($this->auth->guest()) 
         {
-            if($patent != null)
+            $this->app->flash("info", "You must be logged in to do that");
+            $this->app->redirect("/login");
+
+        }else{
+        	$result = [];
+        	$needle  = $this->app->request->post('searchquery');
+            $patent = $this->patentRepository->all();
+
+            $validation = new SearchFormValidation($needle);
+            if ($validation->isGoodToGo()) 
             {
-                $patent->sortByDate();
-                foreach ($patent as $haystack) 
+                if($patent != null)
                 {
-                    if ((preg_match("/\b".$needle."\b/i", $haystack->getTitle())) or (preg_match("/\b".$needle."\b/i", $haystack->getCompany())))
+                    $patent->sortByDate();
+                    foreach ($patent as $haystack) 
                     {
-                        $result[] = $haystack;
+                        if ((preg_match("/\b".$needle."\b/i", $haystack->getTitle())) or (preg_match("/\b".$needle."\b/i", $haystack->getCompany())))
+                        {
+                            $result[] = $haystack;
+                        }
                     }
                 }
             }
+            $this->app->flashNow('error', join('<br>', $validation->getValidationErrors()));
+            
+            $this->render('patents/searchResult.twig', ['patent' => $result]);
         }
-        $this->app->flashNow('error', join('<br>', $validation->getValidationErrors()));
-        
-        $this->render('patents/searchResult.twig', ['patent' => $result]);
     }
 
 
